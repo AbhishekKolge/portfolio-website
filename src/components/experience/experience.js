a2469394952d4c78ed6afd39a783b23c1c2812b7
Abhishek Kolge <83946567+AbhishekKolge@users.noreply.github.com>
2023-12-30 15:36:08 +0530

    Resume link updated

--- src/components/experience/experience.js
+++ src/components/experience/experience.js
@@ -23,7 +23,7 @@ const Experience = () => {
           <LinkSecondary
             ariaLabel='View Full Résumé'
             target='_blank'
-            href='https://drive.google.com/file/d/1TFud_VFT13TZ9Suf5y3Bw6E7IhgwJnwH/view?usp=drive_link'
+            href='https://drive.google.com/file/d/1CeYAG5jUpONmR0ZH_PGbTp1N1QSe4J1p/view?usp=drive_link'
           >
             View Full Résumé
           </LinkSecondary>
